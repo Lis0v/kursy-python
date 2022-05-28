0e93a5c02995b8994db67acec07101193aec3736
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-05-28 15:47:47 +0200

    jhhdfsj

---
+++ 09-imie-nazwisko.py
@@ -0,0 +1,15 @@
+imie="Piotr"
+nazwisko="Nowak"
+imie_i_nazwisko="Imie i nazwisko: "
+imie_i_nazwisko+="Piotr"+" "+"Nowak"
+print(imie_i_nazwisko)
+print("Ilość liter w imieniu",imie,"to:",len(imie))
+
+tekst="Jakiś przykładowy tekst"[::-1]
+print(tekst)
+
+spr=input("Podaj wyraz - sprawdzimy czy jest palindromem: ")
+if spr==spr[::-1]:
+    print("Wyraz jest palindromem")
+else:
+    print("Wyraz nie jest palindromem")
