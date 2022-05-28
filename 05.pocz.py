a37df38d8c5e002d43096c4302721abb4e5dfac6
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-05-28 14:44:07 +0200

    krok

--- 05.pocz.py
+++ 05.pocz.py
@@ -1,6 +1,16 @@
 poczatek=int(input("Podaj początek zakresu: "))
 koniec=int(input("Podaj koniec zakresu: "))
+krok=int(input("Podaj wartość kroku: "))
 suma=0
-for i in range(poczatek, koniec+1):
+for i in range(poczatek,koniec+1,krok):
     suma+=i #suma=suma+i
 print("Suma liczb z przedziału od",poczatek,"do",koniec,"wynosi",suma)
+
+'''
+poczatek=5
+koniec=10
+i=poczatek  i=5
+czy i<koniec+1  5<11 TAK
+suma=suma+i     suma=0+5=5
+i=6
+czy 6<11 TAK suma=5+6=11
