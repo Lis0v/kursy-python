acbfda00d3c157b1a91ada36ec90656eae253ca4
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-05-28 16:58:23 +0200

    bruh

--- 10-lista.py
+++ 10-lista.py
@@ -1,2 +1,12 @@
-lista=["Dowolny tekst",34,-90.7]
-print(lista)
+lista1=list(range(0,101,5))
+lista2=list(range(-3,10,2))
+lista3=[]
+lista3.extend(lista1)
+lista3.extend(lista2)
+for i in lista3:
+    print(i,end=" ")
+print(" ")
+if 5 in lista3:
+    print("Wartość 5 jest na liście.")
+else:
+    print("Wartości 5 nie ma w liście")
