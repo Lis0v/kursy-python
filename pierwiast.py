9582dcce8bad5e341abcd75796efc2abcf2b1922
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-06-09 17:24:27 +0200

    b

---
+++ pierwiast.py
@@ -0,0 +1,6 @@
+import math
+zm1=int(input("Podaj dowolną liczbę - wyznaczymy jej pierwiastek kwadratowy:"))
+def pierwiastek(n):
+    return math.sqrt(n)
+    # return n++0.5
+print("Pierwiastek kwadratowy liczby",zm1,"wynosi",pierwiastek(zm1))
