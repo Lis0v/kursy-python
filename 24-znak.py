2b9c7fa7381dc37ecbacebe693393c776edcea77
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-06-09 17:54:29 +0200

    znaki

---
+++ 24-znak.py
@@ -0,0 +1,10 @@
+for znak in 'Wojtek':
+    if znak=='t':
+        break
+    print("Aktualna litera to:",znak)
+zm=100
+while zm>0:
+    print("Aktualna liczba to:",zm)
+    zm-=1
+    if zm==95:
+        break
