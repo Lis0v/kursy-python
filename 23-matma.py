b357e4a06f5e15c88f9b1c39fbcffdf5f873bad6
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-06-09 17:47:20 +0200

    matma

--- 23-matma.py
+++ 23-matma.py
@@ -1,21 +1,32 @@
 import math
+
 zm1=int(input("Podaj zm1:"))
 zm2=int(input("Podaj zm2:"))
+
 print("1-dodawanie")
 print("2-odejmowanie")
 print("3-mnożenie")
 print("4-dzielenie")
 print("5-potęga")
 print("6-pierwiastek")
+
 flaga=int(input("Wybierz działanie:"))
+
+def dodawanie(a,b):
+    return print("Suma wynosi",a+b)
+
 def odejmowanie(a,b):
     return(print("Różnica wynosi",a-b))
+
 def potega(a,b):
     return(print("Liczba",a,"do potęgi",b,"wynosi",a**b))
+
 def pierwiastek(a,b):
     return(print("Pierwiastek",b,"z liczby",a,"wynosi",a**(1/b)))
 
-if flaga==2:
+if flaga==1:
+    dodawanie(zm1,zm2)
+elif flaga==2:
     odejmowanie(zm1,zm2)
 elif flaga==5:
     potega(zm1,zm2)
