c52a71e2967465462141e526d08a746742be7770
U-DESKTOP-0H3LRUG\Lisia <umbro>
2022-06-09 16:28:51 +0200

    symbol newtona

--- 20-newton.py
+++ 20-newton.py
@@ -4,4 +4,4 @@ def newtona(liczba1,liczba2):
     if liczba2 in(0,liczba1):
         return 1
     return newtona(liczba1-1,liczba2-1)+newtona(liczba1-1,liczba2)
-print(newtona(zm1,zm2))
+print("Prawdopodobienstwo wylosowania",zm2,"liczb sposrod",zm1,"wynosi",newtona(zm1,zm2))
