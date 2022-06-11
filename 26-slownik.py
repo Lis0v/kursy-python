336de6c15c8039294d94bfa1cfa0559b42026413
Lis0v <umbro>
2022-06-11 16:32:30 +0200

    alamakota

--- 26-slownik.py
+++ 26-slownik.py
@@ -7,3 +7,8 @@ slownik["klucz3"]=59
 slownik["klucz4"]=[45,-9,15]
 print(slownik)
 print(slownik["klucz4"])
+slownik["klucz2"]="Ala ma kota"
+print(slownik)
+slownik["klucz2"]=slownik["klucz2"]+" i psa."
+slownik["klucz3"]=slownik["klucz3"]+5
+print(slownik)
