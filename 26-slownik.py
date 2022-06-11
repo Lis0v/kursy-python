2406fc6fb29d29cf850ac5da85f4aa1f69d8562c
Lis0v <umbro>
2022-06-11 16:16:32 +0200

    slownik

---
+++ 26-slownik.py
@@ -0,0 +1,9 @@
+przyklad={"klucz1":"wartosc1","klucz2":"wartosc2","klucz3":"wartosc3"}
+print(przyklad)
+slownik={}
+slownik["klucz1"]="wartosc1"
+slownik["klucz2"]="wartosc2"
+slownik["klucz3"]=59
+slownik["klucz4"]=[45,-9,15]
+print(slownik)
+print(slownik["klucz4"])
